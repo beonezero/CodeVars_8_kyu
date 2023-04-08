b47978c4ed9dc2fe2913c9ebb611a7481b60ea72
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-08 21:44:06 +0300

    Create Cat_years_Dog_years.js

---
+++ Cat_years_Dog_years.js
@@ -0,0 +1,5 @@
+let humanYearsCatYearsDogYears = function(humanYears) {
+  if (humanYears == 1) return [1, 15, 15]
+  if(humanYears == 2) return [2, 24, 24]
+  return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
+}
